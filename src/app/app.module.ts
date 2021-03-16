e8a01ac001179e68bfc0cf877bff7945382a21b3
a.sobol <alexandrina.sobol@isa.utm.md>
2021-03-16 18:21:55 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,48 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { CatsComponent } from './cats/cats.component';
+import { RouterModule } from "@angular/router";
+//import { CatComponent } from './cat/cat.component';
+
+import { DemoNgZorroAntdModule } from './ng-zorro-antd.module';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatsComponent,
+    //CatComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzButtonModule,
+    DemoNgZorroAntdModule,
+    RouterModule.forRoot([
+      { path: "cats", component: CatsComponent}
+    ])
+    
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/cats/cats.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cats',
+  templateUrl: './cats.component.html',
+  styleUrls: ['./cats.component.scss']
+})
+
+export class CatsComponent implements OnInit {
+  listOfColumn = [
+    {
+      title: 'Get Name',
+      compare: null,
+      priority: false
+    },
+    {
+      title: 'Breed',
+      compare: null,
+      priority: false
+    },
+    {
+      title: 'Age',
+      compare: (a: CatItem, b: CatItem) => a.age - b.age,
+      priority: 2
+    },
+    {
+      title: 'Add Date',
+      compare: null,
+      priority: false
+    },
+    {
+      title: 'Update Date',
+      compare: null,
+      priority: false
+    }
+  ];
+  listOfData: CatItem[] = [
+{
+    name: 'Bella' ,
+    breed: 'Abyssinian',
+    age: 12,
+    addDate: '1970-01-09 18:45:09',
+    updateDate: '1970-01-09 18:45:10'
+} 
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+interface CatItem {
+  name: string;
+  breed: string;
+  age: number;
+  addDate: string;
+  updateDate: string
+}
+
